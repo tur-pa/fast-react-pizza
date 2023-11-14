a8f4ee7a4acdb40ed2c18508011c0fd66f65c508
Paweł <pavelturczynski@gmail.com>
2023-11-14 19:32:44 +0100

    styling buttons, inputs, layout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Pablito</div>;
+  return <div className="hidden text-sm font-semibold md:block">Pablito</div>;
 }
 
 export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,14 +8,15 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader></Loader>}
       <Header></Header>
 
-      <main>
-        <h1>Content</h1>
-        <Outlet></Outlet>
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet></Outlet>
+        </main>
+      </div>
 
       <CartOverview></CartOverview>
     </div>
